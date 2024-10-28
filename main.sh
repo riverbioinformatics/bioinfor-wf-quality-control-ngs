143b7569bbd53135feb7cd013e7bdacc68622d3d
Thanh-Giang Nguyen <nttg8100@gmail.com>
2024-10-29 00:35:01 +0700

    use output folder

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@ nextflow run river-quality-control-ngs \
     -profile singularity \
     --reads "workspace/<<fastqs_dir>>/*R{1,2}.fq"
     -workDir "workspace/work" \
-    --outdir "workspace/<<uuid_job_id>>/results"
+    --outdir "workspace/<<output_folder>>"
