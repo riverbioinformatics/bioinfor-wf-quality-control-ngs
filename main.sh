043b5fceb7014d8971d9aac9c29f203f202d65a5
Thanh-Giang Nguyen <nttg8100@gmail.com>
2024-10-29 00:48:22 +0700

    update the main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 nextflow run river-quality-control-ngs \
-    -profile singularity \
+    -profile conda \
     --reads "workspace/<<fastqs_dir>>/*R{1,2}.fq"
     -workDir "workspace/work" \
     --outdir "workspace/<<output_folder>>"
