d04da9ccd002bf8076c688f899d1c5a5b052e9d7
Thanh-Giang Nguyen <nttg8100@gmail.com>
2024-10-30 21:56:46 +0700

    tmp holder

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-nextflow run river-quality-control-ngs \
+nextflow run ./river-quality-control-ngs \
     -profile conda \
-    --reads "workspace/<<fastqs_dir>>/*R{1,2}.fq"
-    -workDir "workspace/work" \
-    --outdir "workspace/<<output_folder>>"
+    --reads "workspace/<<fastqs_dir>>/*{1,2}.fq" \
+    --workDir "workspace/work" \
+    --outdir "workspace/<<output_folder>>/"
