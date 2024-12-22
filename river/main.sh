b9f0f200e542536a7ca5e8ffa0a0698125e8d542
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2024-12-23 00:11:59 +0700

    back to use micromamba due to slurm in docker can not bind proc

--- river/main.sh
+++ river/main.sh
@@ -1,6 +1,6 @@
 sleep 1
 echo "Start analysis"
 nextflow run ./analysis \
-    -profile singularity \
+    -profile conda \
     --reads "workspace<<fastqs_dir>>*{1,2}.fq" \
     --outdir "workspace<<output_folder>>"
